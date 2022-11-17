7fa3ed22e175f19a12884a0abac1b41759f563c2
alex traverso <alextraverso6@gmail.com>
2022-11-17 10:57:38 -0300

    fix navbar styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ return (
     <nav className='navbar navbar-expand-lg navbar-dark bg-primary justify-content-between'>
         <div className="container">
             <h1>
-                <Link to={"/"} className="text-light">CRUD - React, Redux, REST API & AXIOS
+                <Link to={"/"} className="title-nav">CRUD - React, Redux, REST API & AXIOS
                 </Link>
             </h1>
         </div>
