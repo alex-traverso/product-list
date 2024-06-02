5e6943fb52480d27a16f99151cbe631cae2650b3
alex traverso <alextraverso6@gmail.com>
2024-06-02 19:02:51 -0300

    modify product compo

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,66 +5,66 @@ import Swal from "sweetalert2";
 //redux
 import { useDispatch } from "react-redux";
 import {
-  deleteProductAction,
-  getEditProduct,
+	deleteProductAction,
+	getEditProduct,
 } from "../actions/actionsProducts";
 
 const Product = ({ product }) => {
-  const { name, price, id } = product;
-  const dispatch = useDispatch();
-  const navigate = useNavigate(); //habilitar history para redireccion
+	const { name, price, id } = product;
+	const dispatch = useDispatch();
+	const navigate = useNavigate(); //habilitar history para redireccion
 
-  const confirmDeleteProduct = (id) => {
-    //preguntar al usuario
-    Swal.fire({
-      title: "¿Estás seguro?",
-      text: "Un producto que se elimina no se puede recuperar",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Si, eliminar!",
-      cancelButtonText: "Cancelar",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        //pasarlo al action
-        dispatch(deleteProductAction(id));
-      }
-    });
-  };
+	const confirmDeleteProduct = (id) => {
+		//preguntar al usuario
+		Swal.fire({
+			title: "¿Estás seguro?",
+			text: "Un producto que se elimina no se puede recuperar",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Si, eliminar!",
+			cancelButtonText: "Cancelar",
+		}).then((result) => {
+			if (result.isConfirmed) {
+				//pasarlo al action
+				dispatch(deleteProductAction(id));
+			}
+		});
+	};
 
-  // funcion que redirige de forma programada
-  const redirectEdit = (product) => {
-    dispatch(getEditProduct(product));
-    navigate(`/productos/editar/${product.id}`);
-  };
+	// funcion que redirige de forma programada
+	const redirectEdit = (product) => {
+		dispatch(getEditProduct(product));
+		navigate(`/productos/editar/${product.id}`);
+	};
 
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>
-        <span className='font-weight-bold'> $ {price}</span>
-      </td>
-      <td className='actions'>
-        <button
-          type='button'
-          onClick={() => {
-            redirectEdit(product);
-          }}
-          className='btn btn-primary mr-2'
-        >
-          Editar
-        </button>
-        <button
-          type='button'
-          className='btn btn-danger'
-          onClick={() => confirmDeleteProduct(id)}
-        >
-          Eliminar
-        </button>
-      </td>
-    </tr>
-  );
+	return (
+		<tr>
+			<td>{name}</td>
+			<td>
+				<span className="font-weight-bold"> $ {price}</span>
+			</td>
+			<td className="actions">
+				<button
+					type="button"
+					onClick={() => {
+						redirectEdit(product);
+					}}
+					className="btn btn-primary mr-2"
+				>
+					Editar
+				</button>
+				<button
+					type="button"
+					className="btn btn-danger"
+					onClick={() => confirmDeleteProduct(id)}
+				>
+					Eliminar
+				</button>
+			</td>
+		</tr>
+	);
 };
 
 export default Product;
